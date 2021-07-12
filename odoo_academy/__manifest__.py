5cbf811a72fb8a880c6e6e1d219d0e2ed0b81c6f
Gerardo Andrade <gandrade@crucialsoft.com.mx>
2021-07-12 16:53:44 +0000

    [ADD] basic module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'odoo Academy',
+    'summary' : """Academy app training""",
+    'description' :"""
+        Academy curses
+    """,
+    'author' : 'crucialsoft',
+    'category' : 'Training',
+    'version' : '0.0.1',
+    'depends' : ['base'],
+    'data' : [
+        
+    ],
+    'demo': [
+        
+    ],
+
+}
